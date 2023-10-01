0a3817bab73155761940593638c62b1d3ea849ca
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-10-01 20:58:13 +0300

    done done done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 //import logo from "./logo.svg";
 import "./App.css";
-
+import products from "./products";
 function App() {
+  const productsList = products.map((book) => {
+    return (
+      <div>
+        <h1>{book.name}</h1>
+        <h4>{book.price}</h4>
+        <img src={book.img} width={"200px"} />
+      </div>
+    );
+  });
   return (
     <div className="App">
       <h1>Life..</h1>
       <h2>Reading a book adds another life to your life.</h2>
       <img src="https://cdn.al-ain.com/images/2019/3/20/153-041534-house-wisdom-scientific-intellectual-center_700x400.jpeg"></img>
+      {productsList}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    name: "alomr althahb",
+    price: "3.00 KD",
+    img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1633695708l/59240816.jpg",
+  },
+  {
+    name: "Siyar Alam Nubala",
+    price: "75 KD",
+    img: "https://th.bing.com/th/id/OIP.oOEHxNiGOVxy79r6d-QPQwHaK7?w=116&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+  },
+  {
+    name: "Al-dhil w alharror",
+    price: "5 KD",
+    img: "https://ak-asset.jarir.com/akeneo-prod/asset/1/b/4/1/1b4146db18f109fc7f492b0dba911dc52f0d702d_587657.jpg",
+  },
+  {
+    name: "Haiati",
+    price: "3.5 KD",
+    img: "https://th.bing.com/th/id/R.c202a8b4dee43865c89041f3455ecc6c?rik=iYVm5PkXXLagBg&pid=ImgRaw&r=0",
+  },
+  {
+    name: "Haiat fi aledara",
+    price: "3.5KD",
+    img: "https://th.bing.com/th/id/OIP.bgm_3OM1v_CxBAaBGzWHCQHaKg?pid=ImgDet&rs=1",
+  },
+  {
+    name: "traif w mosamrat",
+    price: "4KD",
+    img: "https://www.sifatusafwa.com/22371-medium_default/tara-if-wa-moussamarat-mouhammad-al-bayoumi.jpg",
+  },
+];
+
+export default products;
